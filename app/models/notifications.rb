1a1df3ef68126593269ed32854d3367dbcf70858
Craig Buchek <craig@boochtek.com>
2010-03-28 17:13:10 -0500

    Add notification emailer for contact form.

---
+++ app/models/notifications.rb
@@ -0,0 +1,12 @@
+class Notifications < ActionMailer::Base
+  def contact(params)
+    subject     "#{SITE_NAME} feedback from #{params[:name]}"
+    recipients  EMAIL_FEEDBACK_TO
+    from        params[:email_address]
+    sent_on     Time.now.utc
+
+    body :comment => params[:comment], 
+         :name => params[:name],
+         :email_address => params[:email_address]
+  end
+end
