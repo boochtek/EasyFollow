4fe3d5ea4458554c62a780fc9dfc17f951365688
Craig Buchek <craig@boochtek.com>
2010-04-01 18:31:57 -0500

    Add login steps to make login feature work.

--- features/login.feature
+++ features/login.feature
@@ -6,22 +6,24 @@ Feature: Home Page
 
     Scenario: Logging in with correct username and password
         Given I am not logged in
-        And an account exists with username="test" and password="password"
+        And the "test" account exists
+        And the "test" account has a password of "password"
         When I go to the login page
-        And fill in User Name with "test"
-        And fill in Password with "password"
+        And fill in "User Name" with "test"
+        And fill in "Password" with "password"
         And click on the "Log In" button
-        Then I should be on the home page
+        Then I should end up on the home page
         And I should be logged in
         And I should see my name
         And I should see my profile
 
     Scenario: Trying to log in with incorrect password
         Given I am not logged in
-        And an account exists with username="test" and password="password"
+        And the "test" account exists
+        And the "test" account has a password of "password"
         When I go to the login page
-        And fill in User Name with "test"
-        And fill in Password with "incorrect password"
+        And fill in "User Name" with "test"
+        And fill in "Password" with "incorrect password"
         And click on the "Log In" button
         Then I should still be on the login page
         And I should see "Incorrect username or password"
@@ -31,10 +33,10 @@ Feature: Home Page
 
     Scenario: Trying to log in to account that doesn't exist
         Given I am not logged in
-        And no account exists with username="test"
+        And no "test" account exists
         When I go to the login page
-        And fill in User Name with "test"
-        And fill in Password with "password"
+        And fill in "User Name" with "test"
+        And fill in "Password" with "password"
         And click on the "Log In" button
         Then I should still be on the login page
         And I should see "Incorrect username or password"
@@ -48,4 +50,4 @@ Feature: Home Page
         Then I should be logged out
         And I should be redirected to the home page
         And I should see "logged out"
-        And I should see "create profile"
+        And I should see "Create a profile"
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,53 +1,101 @@
 Given /^(?:|I )(?:am logged|log) in$/ do
   login
 end
-
 Given /^(?:|I )am logged out$/ do
   logout
 end
-
 Given /^(?:|I )am (?:not|NOT) logged in$/ do
   Given 'I am logged out'
 end
 
-Given /^(?:|I )(?:am logged|log) in as (.*)$/ do |username|
+Given /^(?:|I )(?:am logged|log) in as (.+)$/ do |username|
   # TODO: Snip off dquotes, if neccessary.
-  # TODO: Allow looking up user by username of full name.
-  login(username)
+  # TODO: Allow looking up user by username or full name.
+  Given "the \"#{username}\" user exists"
+  login_as(username)
 end
 
-Then /^(?:|I )should see my name$/ do
-  Then 'I should see "Fake User"'
+Then /^(?:|I )should(| not| NOT) see my name$/ do |nt|
+  Then "I should#{nt} see \"Fake User\""
 end
 
-Then /^(?:|I )should see my profile$/ do
-  Then 'I should see "User Profile"'
+Then /^(?:|I )should(| not| NOT) see my profile$/ do |nt|
+  Then "I should#{nt} see \"User Profile\""
 end
+
+
+Given /^the "([^\"]*)" user exists$/ do |username|
+  @user = Factory(:user, :login => username)
+end
+
+
+Then /^I should be logged out$/ do
+  Then 'I should NOT be logged in'
+end
+Then /^I should(| not| NOT) be logged in$/ do |nt|
+  user_id = @integration_session.session[:user_id]
+  if nt =~ / not/i
+    user_id.should be_nil
+  else
+    user_id.should_not be_nil
+  end
+end
+
+Given /^the "([^\"]*)" account exists$/ do |user|
+  Factory(:user, :login => user)
+end
+Given /^no "([^\"]*)" account exists$/ do |user|
+  User.find_by_login(user).should be_nil
+end
+
+Given /^the "([^\"]*)" account has a(?:|n) (.+) of "([^\"]*)"$/ do |user, prop, val|
+  User.find_by_login(user).send(prop, val) unless prop == 'password' # We don't support passwords yet.
+end
+
 
 
 # These are the simplest way to test logging in and out without actually having a User class.
 def login
   visit('/home/fake_login')
+  @integration_session.session[:user_id].should_not be_nil
 end
 
 def logout
   visit('/home/fake_logout')
+  @integration_session.session[:user_id].should be_nil
+end
+
+# This one requires having a User class.
+def login_as(login)
+  user = @user || User.find_by_login(login) || Factory(:user, :login => login)
+  visit("/home/fake_login?id=#{user.id}")
+  user_id = @integration_session.session[:user_id]
+  user_id.should == user.id
 end
 
 
 # Fake out login, based on ideas from http://www.jarrodspillers.com/2008/08/22/faking-the-funk-stub-authentication-in-a-rails-rspec-story/.
 class HomeController
   def fake_login
-    session[:user] = FakeUser.new
+    (user = params[:id] ? User.find(params[:id]) : User.new).save(false)
+    session[:user_id] = user.id
     render :text => ''
   end
   def fake_logout
-    session[:user] = nil
+    session[:user_id] = nil
     render :text => ''
   end
 end
 
-class FakeUser
+if !defined?(User)
+  class User
+    def self.find(id)
+      self.new
+    end
+  end
+end
+
+class User
   def full_name
     'Fake User'
   end
