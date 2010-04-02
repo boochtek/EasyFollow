7db92700fe5b9f2b5f05627e2cb20737b757dc13
Craig Buchek <craig@boochtek.com>
2010-04-02 15:05:05 -0500

    Rename user/new page to user/signup.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,14 @@
 class UserController < ApplicationController
 
+  # Signup page to create a new user account
   def new
     @user = User.new
+    render 'user/signup'
   end
 
   def create
     @user = User.new # TODO
-    render 'user/new'
+    render 'user/signup'
   end
 
 end
