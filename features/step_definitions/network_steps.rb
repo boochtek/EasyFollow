7e4fd15ea8fe62a2ac930b109f2f49f1f97e78db
Craig Buchek <craig@boochtek.com>
2010-04-04 13:38:06 -0500

    Add initial steps for network features.

---
+++ features/step_definitions/network_steps.rb
@@ -0,0 +1,34 @@
+Given /^I have not joined any networks$/ do
+  # TODO
+end
+
+
+When /^I enter my Twitter account info$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I enter incorrect Twitter account info$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+
+When /^I click on "Add Network" next to the "([^\"]*)" network$/ do |network|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see "Add Network" next to each networking site$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+
+Then /^I should see a list of networking sites$/ do
+  Then 'I should see "Twitter"'
+  Then 'I should see "LinkedIn"'
+  Then 'I should see "Facebook"'
+end
+
+
+Then /^I should see my Twitter account name next to the "Twitter" network$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
