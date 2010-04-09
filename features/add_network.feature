b830c185b141b9624e1483ed46560a0b00058ca8
Craig Buchek <craig@boochtek.com>
2010-04-09 18:58:20 -0500

    Update add_network feature and network steps.

--- features/add_network.feature
+++ features/add_network.feature
@@ -9,25 +9,23 @@ Feature: Add Network
         And I have not joined any networks
         When I go to the networks page
         Then I should see a list of networking sites
-        And I should see "Add Network" next to each networking site
+        And I should see "Add Network" for each networking site
 
     Scenario: Add Twitter network
         Given I am logged in
         And I have not joined any networks
         And I go to the networks page
-        When I click on "Add Network" next to the "Twitter" network
-        Then I should end up on the add Twitter network page
-        And I should see "Twitter"
-        When I enter my Twitter account info
+        When I follow "Add Network" within "#Twitter"
+        And I authorize this app to access my Twitter account
         Then I should be redirected to the networks page
         And I should see "Twitter network added"
-        And I should see my Twitter account name next to the "Twitter" network
+        And I should see my Twitter account name within "#Twitter"
 
     Scenario: Trying to add Twitter, but network credentials are incorrect
         Given I am logged in
         And I have not joined any networks
-        And I go to the add Twitter network page
-        And I should see "Twitter"
-        When I enter incorrect Twitter account info
-        Then I should still be on the add network page
+        And I go to the networks page
+        When I follow "Add Network" within "#Twitter"
+        And I do not authorize this app to access my Twitter account
+        Then I should still be on the networks page
         And I should NOT see "Twitter network added"
--- features/step_definitions/network_steps.rb
+++ features/step_definitions/network_steps.rb
@@ -1,34 +1,36 @@
-Given /^I have not joined any networks$/ do
+SITES_TO_CHECK = %w[Twitter] # TODO: Add LinkedIn, Facebook, etc.
+ACCOUNTS_TO_USE = {'Twitter' => 'CraigBuchek'}
+
+Given /^(?:|I )have not joined any networks$/ do
   # TODO
 end
 
 
-When /^I enter my Twitter account info$/ do
-  pending # express the regexp above with the code you wish you had
+When /^(?:|I )authorize this app to access my Twitter account$/ do
+#  pending # express the regexp above with the code you wish you had
+  visit network_oauth_url(:network => 'twitter')
 end
 
-When /^I enter incorrect Twitter account info$/ do
-  pending # express the regexp above with the code you wish you had
-end
-
-
-When /^I click on "Add Network" next to the "([^\"]*)" network$/ do |network|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^I should see "Add Network" next to each networking site$/ do
-  pending # express the regexp above with the code you wish you had
+When /^(?:|I )do not authorize this app to access my Twitter account$/ do
+#  pending # express the regexp above with the code you wish you had
 end
 
 
-Then /^I should see a list of networking sites$/ do
-  Then 'I should see "Twitter"'
-  Then 'I should see "LinkedIn"'
-  Then 'I should see "Facebook"'
+Then /^(?:|I )should see "([^\"]*)" for each networking site$/ do |text|
+  SITES_TO_CHECK.each do |site|
+    Then "I should see \"#{text}\" within \"##{site}\""
+  end
 end
 
 
-Then /^I should see my Twitter account name next to the "Twitter" network$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^(?:|I )should see a list of networking sites$/ do
+  SITES_TO_CHECK.each do |site|
+    Then "I should see \"#{site}\""
+  end
+end
+
+
+Then /^(?:|I )should see my ([^\"]*) account name within "([^\"]*)"$/ do |site, scope|
+  Then "I should see \"#{ACCOUNTS_TO_USE[site]}\" within \"#{scope}\""
 end
 
