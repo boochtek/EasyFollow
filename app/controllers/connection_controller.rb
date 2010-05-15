fd30db811698669e8ae2712d111e9840d0a8b1c2
Craig Buchek <craig@boochtek.com>
2010-05-15 12:31:34 -0500

    Add connection/create action, to make the "Follow Me" button work.

---
+++ app/controllers/connection_controller.rb
@@ -0,0 +1,15 @@
+class ConnectionController < ApplicationController
+
+  def create
+    @user = User.find_first_by_username(params[:user])
+    if @user
+      current_user.follow(@user)
+      redirect_to my_profile_path
+    else
+      # This should not happen unless the followee has disabled their account, or someone is directly POSTing to the URL.
+      # So we don't care if the error message is just plain text.
+      render :text => "You must specify a user to follow!"
+    end
+  end
+
+end
