8b15d87ca3a9e843763778260e645e6e4f3d8432
Craig Buchek <craig@boochtek.com>
2010-05-15 22:19:31 -0500

    Integrate signup page 1 from Dylan's HTML. Fix some feature scenarios that broke with the changes.

--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -7,11 +7,11 @@ end
 
 When /^I fill out the signup form$/ do
   user = Factory.attributes_for(:user)
-  fill_in 'Username', :with => user[:username]
-  fill_in 'First Name', :with => user[:first_name]
-  fill_in 'Last Name', :with => user[:last_name]
-  fill_in 'Email Address', :with => user[:email]
-  fill_in 'Password', :with => user[:password]
+  fill_in 'user[username]', :with => user[:username]
+  fill_in 'user[first_name]', :with => user[:first_name]
+  fill_in 'user[last_name]', :with => user[:last_name]
+  fill_in 'user[email]', :with => user[:email]
+  fill_in 'user[password]', :with => user[:password]
   check 'Agree to Terms'
 end
 
@@ -23,7 +23,7 @@ end
 
 When /^I fill out the signup form incorrectly$/ do
   When 'I fill out the signup form'
-  fill_in 'Email Address', :with => 'invalid_email_address'
+  fill_in 'user[email]', :with => 'invalid_email_address'
   check 'Agree to Terms'
   click_button 'Sign Up'
 end
@@ -32,25 +32,33 @@ end
 When /^I enter a username that is in the restricted list$/ do
   restricted_username = 'logout'
   When 'I fill out the signup form'
-  fill_in 'Username', :with => restricted_username
+  fill_in 'user[username]', :with => restricted_username
   click_button 'Sign Up'
 end
 
 When /^I enter a username that is already taken$/ do
   user = Factory(:user)
   When 'I fill out the signup form'
-  fill_in 'Username', :with => user.username
+  fill_in 'user[username]', :with => user.username
   click_button 'Sign Up'
 end
 
 When /^I enter a username that contains a slash$/ do
   When 'I fill out the signup form'
-  fill_in 'Username', :with => 'craig/buchek'
+  fill_in 'user[username]', :with => 'craig/buchek'
   click_button 'Sign Up'
 end
 
 
-Then /^I should(?:| still) be in "([^\"]*)" of the signup$/ do |step|
+Then /^I should(?:| still) be in "Step ([123])" of the signup$/ do |step|
+  case step.to_i
+  when 1
+    Then %{I should see "Agree to Terms"}
+  when 2
+    Then %{I should see "Select at least one of the Meezy.me supported networks"}
+  when 3
+    Then %{I should see "Agree to Terms"}
+  end
   Then %{I should see "#{step}"}
 end
 
