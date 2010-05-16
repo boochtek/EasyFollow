4d23b906be45bcf010346ca623cee2c1e50d1c99
Craig Buchek <craig@boochtek.com>
2010-05-16 12:03:03 -0500

    Integrate signup page 3 from Dylan's HTML.

--- app/controllers/connection_controller.rb
+++ app/controllers/connection_controller.rb
@@ -4,7 +4,7 @@ class ConnectionController < ApplicationController
     @user = User.find_first_by_username(params[:user])
     if @user
       current_user.follow(@user)
-      redirect_to my_profile_path
+      redirect_to :back
     else
       # This should not happen unless the followee has disabled their account, or someone is directly POSTing to the URL.
       # So we don't care if the error message is just plain text.
