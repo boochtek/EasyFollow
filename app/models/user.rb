5fac1860533ee0946bef6f1e58eb14e9eca83636
Craig Buchek <craig@boochtek.com>
2010-05-16 15:42:33 -0500

    Display bio details in profile.

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,8 @@ class User < ActiveRecord::Base
     end
   end
 
+  delegate :location, :title, :industry, :description, :to => '(bio or return nil)'
+
   validates_each :username do |record, attr, value|
     record.errors.add attr, 'may only contain alphanumeric characters, plus _ . ! @ -' if value !~ %r{\A[-_.!@[:alnum:]]*\Z}
     record.errors.add attr, 'is not allowed' if value =~ PROHIBITED_USERNAME_REGEX
