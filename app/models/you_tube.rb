8b0d9a8b15a994f2eb0a48ad5edc740b48f931a8
Craig Buchek <craig@boochtek.com>
2010-05-23 10:10:16 -0500

    Fix YouTube::get_user_details so that we can add the YouTube network.

--- app/models/you_tube.rb
+++ app/models/you_tube.rb
@@ -12,9 +12,9 @@ class YouTube < OAuthSite
       # Get the user's profile information.
       response = access_token(account).get('http://gdata.youtube.com/feeds/api/users/default')
       xml = Nokogiri::XML(response.body)
-      username   = xml.xpath('//entry/yt:userame/text()',   {'yt' => 'http://gdata.youtube.com/schemas/2007'}).to_s
-      first_name = xml.xpath('//entry/yt:firstName/text()', {'yt' => 'http://gdata.youtube.com/schemas/2007'}).to_s
-      last_name  = xml.xpath('//entry/yt:lastName/text()',  {'yt' => 'http://gdata.youtube.com/schemas/2007'}).to_s
+      username   = xml.xpath('//yt:username/text()',   {'yt' => 'http://gdata.youtube.com/schemas/2007'}).to_s
+      first_name = xml.xpath('//yt:firstName/text()', {'yt' => 'http://gdata.youtube.com/schemas/2007'}).to_s
+      last_name  = xml.xpath('//yt:lastName/text()',  {'yt' => 'http://gdata.youtube.com/schemas/2007'}).to_s
 
       # Return the user details.
       return {:username => username, :full_name => "#{first_name} #{last_name}"}
@@ -22,7 +22,6 @@ class YouTube < OAuthSite
 
     # Have one user follow another on this site. (Or whatever is most analagous to following.)
     def follow(account, account_to_follow)
-      xml = Nokogiri::XML(response.body)
       xml = <<-"XML"
         <?xml version="1.0" encoding="UTF-8"?>
         <entry xmlns="http://www.w3.org/2005/Atom"
