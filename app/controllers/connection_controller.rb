26ac6ccfeeb08bd920f696a1f43f8bc5df4317c7
Craig Buchek <craig@boochtek.com>
2010-06-11 09:38:30 -0500

    Allow per-network unfollowing from other user's profile.

--- app/controllers/connection_controller.rb
+++ app/controllers/connection_controller.rb
@@ -14,13 +14,13 @@ class ConnectionController < ApplicationController
 
   def destroy
     @user = User.find_first_by_username(params[:user])
-    if @user and current_user and current_user.following?(@user)
-      current_user.unfollow(@user)
+    if @user and current_user and current_user.following?(@user, params[:network])
+      current_user.unfollow(@user, params[:network])
       redirect_to :back
     else
       # This should not happen unless someone is directly POSTing to the URL.
       # So we don't care if the error message is just plain text.
-      render :text => "Cannot delete that connection! #{current_user.id} #{@user.id} #{current_user.following?(@user)}"
+      render :text => "Cannot delete that connection! #{current_user.username} #{@user.username} #{params[:network]} #{current_user.following?(@user, params[:network])}"
     end
   end
 
