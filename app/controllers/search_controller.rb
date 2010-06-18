bf493f225ac50cba5c368ab0a8befa67ae19bdef
Craig Buchek <craig@boochtek.com>
2010-06-17 22:09:45 -0500

    Divide search results into in-network and out-of-network for logged-in users.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,10 @@ class SearchController < ApplicationController
 
   def index
     @users = User.search(params[:query], params)
+    if current_user
+      @users_in_my_network = @users.select{|user| current_user.following?(user)}
+      @users_not_in_my_network = @users.reject{|user| current_user.following?(user)}
+    end
   end
 
 end
